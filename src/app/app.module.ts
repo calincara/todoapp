27392a215e35a9941cc424445b32d336ea478f0e
Calin Cara <Calin.Cara@msg.group>
2017-10-11 18:28:18 +0300

    App header added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-import { MatButtonModule } from '@angular/material';
+import { matModules } from './modules/material-design.module';
+import { HeaderComponent } from './header/header.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        HeaderComponent
     ],
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
-        MatButtonModule
+        ...matModules,
+        FlexLayoutModule
     ],
     providers: [],
     bootstrap: [AppComponent]
---
+++ src/app/modules/material-design.module.ts
@@ -0,0 +1,13 @@
+import {
+    MatToolbarModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatSidenavModule
+} from '@angular/material';
+
+export const matModules = [
+    MatToolbarModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatSidenavModule
+];
